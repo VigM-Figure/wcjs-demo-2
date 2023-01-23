832f45fd3992fa5bf914ca08fb5f76d2e47de2c6
VigM <vmikulis@figure.com>
2023-01-23 16:39:06 -0700

    found issue I think

--- src/App.tsx
+++ src/App.tsx
@@ -10,47 +10,36 @@ import "./App.css";
 function App() {
   const [isOpen, setIsOpen] = useState(false);
   const [error, setError] = useState("");
+  const [bridge, setBridge] = useState(
+    "wss://test.figure.tech/service-wallet-connect-bridge/ws/external"
+  );
   const [signingComplete, setSigningComplete] = useState(false);
   const [result, setResult] = useState<BroadcastResult | "">("");
   const { walletConnectService, walletConnectState } = useWalletConnect();
-  const { connected, connector } = walletConnectState;
-  const [walletconnectStorage, setWalletConnectStorage] = useState<
-    typeof connector | null
-  >();
-  // Mock data
-  const mockConnector = {
-    connected: true,
-    accounts: [
-      {
-        publicKey: "A5uhrTePwwKJVnOqwYGMGSnPVE8QIKF76iR+5x7RgZOU",
-        address: "tp1ka6dv5lmm50g0n7lqhk29dj98ra876d2ajh4g6",
-        jwt: "eyJhbGciOiJFUzI1NksiLCJ0eXAiOiJKV1QifQ.eyJzdWIiOiJBNXVoclRlUHd3S0pWbk9xd1lHTUdTblBWRThRSUtGNzZpUis1eDdSZ1pPVSIsImlzcyI6InByb3ZlbmFuY2UuaW8iLCJpYXQiOjE2NzQ1MDgyOTksImV4cCI6MTY3NDU5NDY5OSwiYWRkciI6InRwMWthNmR2NWxtbTUwZzBuN2xxaGsyOWRqOThyYTg3NmQyYWpoNGc2In0.1qAMY9g9ebUrOSU5hM2vPQyOHN6sCJEVKsfd3pNzNXxWwHBQQmXlYd1yBDpSrtaRaT7FuTsPQ1ARyv9oC3nIzA",
-        walletInfo: { id: "id", name: "MorleyAccount01", coin: "coin" },
-      },
-    ],
-    chainId: "pio-testnet-1",
-    bridge: "wss://figure.tech/service-wallet-connect-bridge/ws/external",
-    key: "4b353453f56253667ca067dc52bf1a0435c793d8de719138f8b6f81084f9a880",
-    clientId: "c86a81a2-619c-4bbd-b404-120c26541e56",
-    clientMeta: {
-      description: "Web site created using create-react-app",
-      url: "http://localhost:3000",
-      icons: [
-        "http://localhost:3000/favicon.ico",
-        "http://localhost:3000/logo192.png",
-      ],
-      name: "React App",
-    },
-    peerId: "bb0724fa-cd70-40ba-91ed-ce082817f58f",
-    peerMeta: {
-      description: "Figure Wallet",
-      url: "chrome-extension://bdggkhpjhpejibpfjdcnnnmbkgliffmi",
-      icons: [],
-      name: "Figure Wallet",
-    },
-    handshakeId: 1674508295846246,
-    handshakeTopic: "5270089f-0331-4f0e-bde9-37a52b4474d7",
-  };
+  const {
+    connected,
+    address,
+    walletInfo,
+    bridge: connectedBridge,
+    connector,
+  } = walletConnectState;
+  const bridgeOptions = [
+    "wss://www.figure.tech/service-wallet-connect-bridge/ws/external",
+    "wss://test.figure.tech/service-wallet-connect-bridge/ws/external",
+    "https://bridge.walletconnect.org",
+    "wss://bridge.aktionariat.com:8887",
+  ];
+
+  useEffect(() => {
+    // Onload, log the current connector
+    console.log("App.tsx | useEffect() | connector: ", connector);
+  }, [connector]);
+  useEffect(() => {
+    console.log(
+      "App.tsx | useEffect() | walletConnectState: ",
+      walletConnectState
+    );
+  }, [walletConnectState]);
 
   // Attempt to re-create Matt H. wcjs issue here
   useEffect(() => {
@@ -77,7 +66,7 @@ function App() {
   }, [isOpen, walletConnectService]);
 
   const handleConnect = () => {
-    walletConnectService.connect();
+    walletConnectService.connect({ bridge });
   };
   const handleDisconnect = () => {
     walletConnectService.disconnect();
@@ -85,39 +74,56 @@ function App() {
   const toggleIsOpen = () => {
     setIsOpen(!isOpen);
   };
-  const addMockToStorage = () => {
-    window.localStorage.setItem("walletconnect", JSON.stringify(mockConnector));
-  };
-  const getStorageData = () => window.localStorage.getItem("walletconnect");
-
-  const showStorageHandshakeTopic = () => {
-    const existingConnectorData = getStorageData();
-    const parsedConnectorData = JSON.parse(existingConnectorData || "{}");
-    setWalletConnectStorage(parsedConnectorData);
-  };
 
   return (
-    <div className="App">
+    <div className="App" style={{ textAlign: "left" }}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>WCJS TEST APP</h1>
-        {/* <div>Mock handshakeTopic: {mockConnector.handshakeTopic}</div>
-        <div>
-          Connector handshakeTopic: {connector?.handshakeTopic || "N/A"}
-        </div>
-        <button onClick={showStorageHandshakeTopic}>
-          Get latest storage handshakeTopic
-        </button>
-        <div>
-          Storage handshakeTopic:{" "}
-          {walletconnectStorage?.handshakeTopic || "N/A"}
-        </div> */}
-        {connected ? (
+        {connected && (
           <div>
             <div>Connected!</div>
-            <button onClick={handleConnect}>connect again (break me)</button>
-            <button onClick={handleDisconnect}>disconnect</button>
-            <button onClick={toggleIsOpen}>isOpen: {`${isOpen}`}</button>
+            <hr />
+            <div>
+              <h3>WalletConnectState:</h3>
+              <div>
+                <b>Wallet Name: </b>
+                {walletInfo.name || "N/A"}
+              </div>
+              <div>
+                <b>Address: </b>
+                {address || "N/A"}
+              </div>
+              <div>
+                <b>Bridge: </b>
+                {connectedBridge || "N/A"}
+              </div>
+              <hr />
+              <h3>Connector:</h3>
+              <div>
+                <b>Bridge: </b>
+                {connector?.bridge || "N/A"}
+              </div>
+              <div>
+                <b>Handshake Topic: </b>
+                {connector?.handshakeTopic || "N/A"}
+              </div>
+              <div>
+                <b>Handshake ID: </b>
+                {connector?.handshakeId || "N/A"}
+              </div>
+              <div>
+                <b>Client ID: </b>
+                {connector?.clientId || "N/A"}
+              </div>
+              <div>
+                <b>Peer ID: </b>
+                {connector?.peerId || "N/A"}
+              </div>
+            </div>
+            <hr />
+            <button onClick={handleDisconnect}>Disconnect</button>
+            <button onClick={toggleIsOpen}>Sign Message</button>
             {error && <div>Error: {error}</div>}
             {signingComplete && <div>Signing Complete!</div>}
             {result && (
@@ -133,14 +139,21 @@ function App() {
               </div>
             )}
           </div>
-        ) : (
-          <div>
-            <button onClick={handleConnect}>connect</button>
-          </div>
         )}
-        {/* <button onClick={addMockToStorage}>
-          fill mock connector to storage
-        </button> */}
+        <div>
+          <div>New Connection Bridge</div>
+          <select
+            value={bridge}
+            onChange={({ target }) => setBridge(target.value)}
+          >
+            {bridgeOptions.map((bridgeUrl) => (
+              <option key={bridgeUrl}>{bridgeUrl}</option>
+            ))}
+          </select>
+          <div>
+            <button onClick={handleConnect}>Connect</button>
+          </div>
+        </div>
       </header>
       <QRCodeModal walletConnectService={walletConnectService} />
     </div>
